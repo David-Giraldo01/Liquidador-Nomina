f1b695f86b51486dbb1fa16bec90ffc9270cd24f
samuel garcia <samuelgarcia201586@gmail.com>
2026-08-28 08:48:04 -0500

    agregar typehints y corregir validaciones

--- src/model/logica_nomina.py
+++ src/model/logica_nomina.py
@@ -6,25 +6,25 @@ from src.model.constantes import (
 from src.model.validacion import ValidadorNomina
 
 
-def calcular_salario_proporcional(salario, dias):
+def calcular_salario_proporcional(salario: float, dias: int )-> float:
     return salario / DIAS_MES * dias
 
 
-def calcular_total_devengado(salario_proporcional, bonificacion, comision):
+def calcular_total_devengado(salario_proporcional: float, bonificacion: float, comision: float)-> float:
     return salario_proporcional + bonificacion + comision
 
 
-def calcular_total_deducciones(total_devengado, descuentos):
+def calcular_total_deducciones(total_devengado: float, descuentos: float)-> float:
     descuento_salud = total_devengado * TASA_DESCUENTO_SALUD
     descuento_pension = total_devengado * TASA_DESCUENTO_PENSION
     return descuento_salud + descuento_pension + descuentos
 
 
-def calcular_neto_pagar(total_devengado, total_deducciones):
+def calcular_neto_pagar(total_devengado: float , total_deducciones: float)-> float:
     return total_devengado - total_deducciones
 
 
-def calcular_nomina(salario, dias, bonificacion, comision, descuentos):
+def calcular_nomina(salario:float, dias: int, bonificacion: float, comision: float, descuentos:float)-> float:
     ValidadorNomina(salario, dias, descuentos).validar()
 
     salario_proporcional = calcular_salario_proporcional(salario, dias)
--- src/model/validacion.py
+++ src/model/validacion.py
@@ -9,24 +9,24 @@ from src.model.errores import (
 class ValidadorNomina:
     """Valida las entradas de la liquidación antes de calcular."""
 
-    def __init__(self, salario, dias, descuentos):
+    def __init__(self, salario, dias, descuentos: float)-> None:
         self.salario = salario
         self.dias = dias
         self.descuentos = descuentos
 
-    def validar(self):
+    def validar(self)-> None:
         self._validar_dias()
         self._validar_salario()
         self._validar_descuentos()
 
-    def _validar_dias(self):
+    def _validar_dias(self)-> None:
         if self.dias < 0 or self.dias > DIAS_MES:
             raise ErrorDiasInvalidos(self.dias)
 
-    def _validar_salario(self):
+    def _validar_salario(self)-> None:
         if self.salario < 0:
             raise ErrorSalarioNegativo(self.salario)
 
-    def _validar_descuentos(self):
+    def _validar_descuentos(self)-> None:
         if self.descuentos < 0:
             raise ErrorDescuentosNegativos(self.descuentos)
